1f4cd639cf6b60e2c95255952b9ca1be42a68131
moroni210 <moronigamesfsa@gmail.com>
2026-06-13 11:04:59 -0300

    OS VERDADEIROS PHP NA PONTA A OUTRA SEM ERRO NENHUM OK

--- ContaPoupanca.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class poupanca{
-    public function renderJuros(ContaBancaria1 $conta, float $taxaJuros){
-        $juros = $conta->getSaldo() * ($taxaJuros / 100);
-        $conta->depositar($juros);
-        echo "Os juros de R$ " . $juros . " foram renderidos com sucesso!!\n\n ";
-    }
-}
